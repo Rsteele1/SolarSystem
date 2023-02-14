39eabc705736200861f409d7836b49fc8e40dd35
Ricketia Steele <ricketiasteele@DoEM-C02FM2DYQ05D.local>
2023-02-13 18:06:29 -0600

    first commit

---
+++ Solar System Swift 2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Solar System Swift 2
+//
+//  Created by Ricketia Steele on 2/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Solar System Swift 2/Solar_System_Swift_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Solar_System_Swift_2App.swift
+//  Solar System Swift 2
+//
+//  Created by Ricketia Steele on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Solar_System_Swift_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
